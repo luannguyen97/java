ab0f001bbe05344ab48e92b72eb6541237fbb632
Luan Nguyen <nguyennhatluan97@gmail.com>
2018-04-11 10:44:33 +0700

    11/4

---
+++ Bai2SW/src/program.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author luannguyen
+ */
+public class program {
+    public static void main(String arg[])
+    {
+        new login().setVisible(true);
+    }
+}
