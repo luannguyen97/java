d01da923e30268ba6ff0fa32a94ba25917d47231
Luan Nguyen <nguyennhatluan97@gmail.com>
2018-04-18 10:12:12 +0700

    Jlist3

---
+++ Bai3SW/src/bai3sw/Bai3SW.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai3sw;
+
+/**
+ *
+ * @author luannguyen
+ */
+public class Bai3SW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new Jlist3().setVisible(true);
+    }
+    
+}
